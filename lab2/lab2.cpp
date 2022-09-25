1e96f430352f03eb85ce39e8c0f286226dd5eb71
sunnehh <sunny_qi@live.ca>
2022-09-25 17:55:45 -0400

    done part 1

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -10,13 +10,39 @@
 /* remember to code these in a RECURSIVE only manner*/
 
 unsigned long long factorial (unsigned int n){
-
+	// base case 0! = 1;
+	if (n == 0) {
+		return 1;
+	}
+	else
+	{
+		return n*factorial(n - 1);
+	}
 }
 
 unsigned long long power (unsigned int base, unsigned int n){
-
-
+	// base case
+	if (n == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return base * power(base, n - 1);
+	}
 }
 unsigned long long fibonacci (unsigned int n){
-
+	//base case 1
+	if (n == 0)
+	{
+		return 0;
+	}
+	//base case 2;
+	else if (n == 1) {
+		return 1;
+	}
+	else
+	{
+		return fibonacci(n - 2) + fibonacci(n - 1);
+	}
 }
